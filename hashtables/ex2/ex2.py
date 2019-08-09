6f4c4252de75bbdb86e91fb33ad7ec0cbece6026
Steve Lanier <stevekentlanier@gmail.com>
2019-08-09 12:19:31 -0700

    hash table 2 complete

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,18 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    #insert all ticket k:v pairs into hashtable
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+    #find first flight by searching the table for the key with a value of NONE
+    first_flight = hash_table_retrieve(hashtable, 'NONE')
+    
+    #insert first flight into route array at index 0
+    route[0] = first_flight
+
+    #for the length of the array (excluding our first flight), assign next index as next flight
+    for i in range(len(route) - 1):
+        route[i + 1] = hash_table_retrieve(hashtable, route[i])
 
     return route
