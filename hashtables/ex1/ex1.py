95b2cb05549c264e7bbabdfa6c588991ad2abce6
Steve Lanier <stevekentlanier@gmail.com>
2019-08-09 11:40:48 -0700

    hash table 1 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -5,13 +5,23 @@ from hashtables import (HashTable,
                         hash_table_retrieve,
                         hash_table_resize)
 
-
+# get indices function
 def get_indices_of_item_weights(weights, length, limit):
+
+    #create hash table
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    #for every weight given, make an index of keys
+    for i in range(0, len(weights)):
+        hash_table_insert(ht, weights[i], i)
+
+    #for every key in the table, assign a value
+    for i in range(0, len(weights)):
+        value = hash_table_retrieve(ht, limit - weights[i])
+
+        #because we have assigned all our values, we have no collisions
+        if value != None:
+            return (value, i)
 
     return None
 
